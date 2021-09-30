6ad221639687c351fa8ed5537f1738355cc78803
Aaron Hargrove <aaronmh98@outlook.com>
2021-09-29 19:33:56 -0500

    Added (very) rough scaffold of application

---
+++ src/components/Game.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+const Game: React.FC = () => {
+  return (
+    <Grid container>
+      <Grid item xs={1} style={{ background: "red" }}>
+        users
+      </Grid>
+      <Grid container direction="column" item xs={10} height="100vh">
+        <Grid item xs={1} style={{ background: "green" }}>
+          navigation
+        </Grid>
+        <Grid item xs={11} style={{ background: "green" }}>
+          <iframe
+            title="wikipedia-content"
+            src="https://en.wikipedia.org/wiki/Killing_of_Muhammad_al-Durrah"
+            style={{ height: "100%", width: "100%" }}
+            frameBorder="0"
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={1} style={{ background: "red" }}>
+        page history
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Game;
---
+++ src/components/Landing.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+    return <div>Hello, I'm a fancy landing page</div>
+}
+
+export default Landing;
